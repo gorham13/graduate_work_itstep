f0dda1739bfbfefd71ff0040efee4f83aa246942
Igor <igorgorham@gmail.com>
2017-07-14 23:21:26 +0300

    added some functions to OrientationController

--- graduate_work_itstep/Controllers/Tests/OrientationController.cs
+++ graduate_work_itstep/Controllers/Tests/OrientationController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Web.Routing;
 
 namespace graduate_work_itstep.Controllers.Tests
 {
@@ -14,12 +16,55 @@ namespace graduate_work_itstep.Controllers.Tests
         // GET: Orientation
         public ActionResult Index()
         {
-            
+            var questions = db.OrientationQuestions.Include(q => q.Answers);
+            return View(questions.ToList());
+        }
 
-            var questions = db.OrientationQuestions;
-            var answers = db.OrientationAnswers;
-            ViewBag.answers = answers;
-            return View(questions);
+        [HttpPost]
+        public RedirectToRouteResult result(string[] positive, string[] negative)
+        {
+            string str = "";
+            int i = 0;
+            int work = 0;
+            int friend = 0;
+            for(int j = 0; j < positive.Length; ++j)
+            {
+                if(positive[j] == "я")
+                {
+                    i += 2;
+                    if (negative[j] == "д")
+                        ++friend;
+                    else
+                        ++work;
+                }
+
+                if (positive[j] == "о")
+                {
+                    friend += 2;
+                    if (negative[j] == "д")
+                        ++i;
+                    else
+                        ++work;
+                }
+
+                if (positive[j] == "д")
+                {
+                    work += 2;
+                    if (negative[j] == "я")
+                        ++friend;
+                    else
+                        ++i;
+                }
+            }
+            return RedirectToAction("result", "Orientation", new { i = i, work = work, friends = friend });
+        }
+        [HttpGet]
+        public string result(int i, int work, int friends)
+        {
+            ViewBag.i = i;
+            ViewBag.work = work;
+            ViewBag.friends = friends;
+            return "work="+work+" i="+i+" friend="+friends;
         }
     }
 }
